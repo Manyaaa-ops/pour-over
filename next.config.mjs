1192a8aaf5df8a0bf001a166bbbf3e4293937804
MANYA DIXIT <manyadixit@MANYAs-MacBook-Air-4.local>
2026-05-07 16:36:45 +0530

    Add vercel config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
